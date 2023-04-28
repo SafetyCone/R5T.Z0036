ab528babe995b8fa0dbd22d7736641391804a53d
davidcoats <david@getsafetycone.com>
2023-04-28 16:57:00 -0700

    Interim changes.

--- source/R5T.Z0036/Code/Values/ICommitMessages.cs
+++ source/R5T.Z0036/Code/Values/ICommitMessages.cs
@@ -1,8 +1,8 @@
 using System;
 
 using R5T.T0131;
-using R5T.T0166;
-using R5T.T0166.Extensions;
+using R5T.T0188;
+using R5T.T0188.Extensions;
 
 
 namespace R5T.Z0036
@@ -13,19 +13,19 @@ namespace R5T.Z0036
     [ValuesMarker]
     public partial interface ICommitMessages : IValuesMarker
     {
-        public CommitMessage AddGitIgnoreFile => "Add .gitignore file.".ToCommitMessage();
-        public CommitMessage AddInitialSolution => "Add initial solution.".ToCommitMessage();
+        public ICommitMessage AddGitIgnoreFile => "Add .gitignore file.".ToCommitMessage();
+        public ICommitMessage AddInitialSolution => "Add initial solution.".ToCommitMessage();
         // Ok, message does not follow "This commit will X" format.
-        public CommitMessage InitialCommit => "Initial commit.".ToCommitMessage();
+        public ICommitMessage InitialCommit => "Initial commit.".ToCommitMessage();
         // Ok, message does not follow "This commit will X" format.
-        public CommitMessage InitialSourceStubCommit => "Initial commit of source-stub.".ToCommitMessage();
+        public ICommitMessage InitialSourceStubCommit => "Initial commit of source-stub.".ToCommitMessage();
         // Ok, message does not follow "This commit will X" format.
-        public CommitMessage InterimChanges => "Interim changes.".ToCommitMessage();
-        public CommitMessage RemoveExtraneousFilesDueToMissingGitIgnore => "Remove extraneous files that should have been ignored, due to a missing .gitignore file.".ToCommitMessage();
+        public ICommitMessage InterimChanges => "Interim changes.".ToCommitMessage();
+        public ICommitMessage RemoveExtraneousFilesDueToMissingGitIgnore => "Remove extraneous files that should have been ignored, due to a missing .gitignore file.".ToCommitMessage();
 
-        public CommitMessage ChangeProjectToNet6 => "Changed project to .NET 6.".ToCommitMessage();
-        public CommitMessage ChangeProjectToNetStandard2_0 => "Change project to .NET Standard 2.0.".ToCommitMessage();
-        public CommitMessage ChangeProjectToNetStandard2_1 => "Change project to .NET Standard 2.1.".ToCommitMessage();
-        public CommitMessage UpgradeSolutionFileToVS2022 => "Upgrade solution file to Visual Studio 2022.".ToCommitMessage();
+        public ICommitMessage ChangeProjectToNet6 => "Changed project to .NET 6.".ToCommitMessage();
+        public ICommitMessage ChangeProjectToNetStandard2_0 => "Change project to .NET Standard 2.0.".ToCommitMessage();
+        public ICommitMessage ChangeProjectToNetStandard2_1 => "Change project to .NET Standard 2.1.".ToCommitMessage();
+        public ICommitMessage UpgradeSolutionFileToVS2022 => "Upgrade solution file to Visual Studio 2022.".ToCommitMessage();
     }
 }
